fbda613fcf5dbb8e56faaf0ea591a54a3d22a56e
Koichi Tamura <hohehohe2@gmail.com>
2014-04-20 14:15:23 +0900

    Particles.cpp implementation improved.

--- geo/Particles.h
+++ geo/Particles.h
@@ -18,24 +18,64 @@ public:
 	typedef std::shared_ptr < Particles > SPtr;
 
 	///Constructor.
-	Particles(const std::string& name="", unsigned int size=0, MemoryType allocMemoryType=BufferSet::HOST) : BufferSetSameSizedChildren(name)
+	/**
+	Only position is added to a child by default. Other children are added if specified as the parameters.
+	**/
+	Particles(const std::string& name="",
+		bool ppPos=false, bool pPos=false, bool velocity=false, bool mass=false, bool force=false, bool pressure=false, bool density=false,
+		unsigned int size=0, MemoryType allocMemoryType=BufferSet::HOST)
+		: BufferSetSameSizedChildren(name)
 	{
-		addChild(m_ppPos = new Points("particle ppPos"));
-		addChild(m_pPos = new Points("particle pPos"));
-		addChild(m_pos = new Points("particle pos"));
-		addChild(m_velocity = new BufferFloat("particle velocity"));
-		addChild(m_mass = new BufferFloat("particle mass"));
-		addChild(m_force = new Points("particle force"));
-		addChild(m_pressure = new BufferFloat("particle pressure"));
-		addChild(m_density = new BufferFloat("particle density"));
-		setSize(size);
-		allocate(allocMemoryType);
+		createChildren(ppPos, pPos, velocity, mass, force, pressure, density, allocMemoryType);
 	}
 
 	///Destructor.
 	virtual ~Particles(){}
 
 	virtual unsigned int size() const {return m_pos->size();}
+
+	///Create children.
+	void createChildren(bool ppPos, bool pPos, bool velocity, bool mass, bool force, bool pressure, bool density,
+		unsigned int size=0, MemoryType allocMemoryType=BufferSet::HOST)
+	{
+		if (ppPos)
+		{
+			removeChild(m_ppPos);
+			addChild(m_ppPos = new Points("particle ppPos"));
+		}
+		if (pPos)
+		{
+			removeChild(m_pPos);
+			addChild(m_pPos = new Points("particle pPos"));
+		}
+		if (velocity)
+		{
+			removeChild(m_velocity);
+			addChild(m_velocity = new BufferFloat("particle velocity"));
+		}
+		if (mass)
+		{
+			removeChild(m_mass);
+			addChild(m_mass = new BufferFloat("particle mass"));
+		}
+		if (force)
+		{
+			removeChild(m_force);
+			addChild(m_force = new Points("particle force"));
+		}
+		if (pressure)
+		{
+			removeChild(m_pressure);
+			addChild(m_pressure = new BufferFloat("particle pressure"));
+		}
+		if (density)
+		{
+			removeChild(m_density);
+			addChild(m_density = new BufferFloat("particle density"));
+		}
+		setSize(size);
+		allocate(allocMemoryType);
+	}
 
 public:
 
