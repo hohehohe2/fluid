be66673a4358977f2e03a1d2d6872347378d94b7
Koichi Tamura <hohehohe2@gmail.com>
2014-04-21 07:41:18 +0900

    Just main.cpp simplified.

--- main.cpp
+++ main.cpp
@@ -9,12 +9,6 @@ using namespace hohehohe2;
 int main()
 {
 	FluidSolverSimpleSph ssph;
-	Lines* lines1 = new Lines;
-	Lines* lines2 = new Lines;
-	std::shared_ptr < BufferSet > sptr1 = lines1->getSelfSptr();
-	std::shared_ptr < BufferSet > sptr2 = lines2->getSelfSptr();
-	*lines2 = *lines1;
-	sptr1.reset();
 
 	return 0;
 }
