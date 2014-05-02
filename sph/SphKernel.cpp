88949548ad75aeb6fdae19e3777ce38bc07b9d16
Koichi Tamura <hohehohe2@gmail.com>
2014-05-02 18:25:20 +0900

    Kernel poly6->cubic spline.

--- sph/SphKernel.cpp
+++ sph/SphKernel.cpp
@@ -15,16 +15,41 @@ void SphKernel::setKernelRadius(float radius)
 	m_r9 = m_r * m_r * m_r * m_r * m_r * m_r * m_r * m_r * m_r;
 }
 
+////---------------------------------------------------------------------------
+////---------------------------------------------------------------------------
+//float SphKernel::w(float dist2) const
+//{
+//	//wPoly6.
+//
+//	if (dist2 < m_r * m_r)
+//	{
+//		float l = m_r * m_r - dist2;
+//		return (float)(315.0 / 64.0 / M_PI / m_r9 * l * l * l);
+//	}
+//	else
+//	{
+//		return 0.0f;
+//	}
+//}
+
+
 //---------------------------------------------------------------------------
 //---------------------------------------------------------------------------
 float SphKernel::w(float dist2) const
 {
-	//wPoly6.
+	//Cubic spline.
 
 	if (dist2 < m_r * m_r)
 	{
-		float l = m_r * m_r - dist2;
-		return (float)(315.0 / 64.0 / M_PI / m_r9 * l * l * l);
+		const float q = sqrt(dist2) / m_r;
+		if (q < 0.5f)
+		{
+			return 8.0f / ((float)M_PI * m_r * m_r * m_r) * (1.0f - 6.0f * q * q + 6 * q * q * q);
+		}
+		else
+		{
+			return 8.0f / ((float)M_PI * m_r * m_r * m_r) * 2 * ( 1.0f - q ) * ( 1.0f - q ) * ( 1.0f - q );
+		}
 	}
 	else
 	{
@@ -37,9 +62,9 @@ float SphKernel::w(float dist2) const
 //---------------------------------------------------------------------------
 void SphKernel::gradW(float result[3], float px, float py, float pz, float nx, float ny, float nz) const
 {
-	float dx = px - nx;
-	float dy = py - ny;
-	float dz = pz - nz;
+	const float dx = px - nx;
+	const float dy = py - ny;
+	const float dz = pz - nz;
 
 	const float dist2 = dx * dx + dy * dy + dz * dz;
 
@@ -54,8 +79,8 @@ void SphKernel::gradW(float result[3], float px, float py, float pz, float nx, f
 
 	const float dist  = sqrt(dist2);
 
-	float l = m_r - dist;
-	float gradWSpiky = (float)(-45.0 / M_PI / m_r6 * l * l);
+	const float l = m_r - dist;
+	const float gradWSpiky = (float)(-45.0 / M_PI / m_r6 * l * l);
 	result[0] = gradWSpiky * dx / dist;
 	result[1] = gradWSpiky * dy / dist;
 	result[2] = gradWSpiky * dz / dist;
@@ -74,6 +99,6 @@ float SphKernel::laplaceW(const float dist2) const
 		return 0.0f;
 	}
 
-	float l = m_r - dist2;
+	const float l = m_r - dist2;
 	return (float)(45.0 / M_PI / m_r6 * l);
 }
